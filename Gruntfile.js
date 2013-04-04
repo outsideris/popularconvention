63adf53f022df38d3b87ae5e6c55999636f752cf
Outsider <outsideris@gmail.com>
2013-04-04 21:52:29 +0900

    initialized node.js project

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      pkg: grunt.file.readJSON('package.json'),
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      src: {
+        src: ['server.js', 'src/**/*.js']
+      },
+      test: {
+        src: ['test/**/*.js']
+      },
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      src: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:src', 'jshint']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'test']
+      },
+    },
+    simplemocha: {
+      all: {
+        src: '<%= jshint.test.src %>',
+        options: {
+          timeout: 3000,
+          ignoreLeaks: false,
+          ui: 'bdd',
+          reporter: 'spec'
+        }
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'simplemocha']);
+  grunt.registerTask('test', ['simplemocha']);
+
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
+# popularconvention [![Build Status](https://secure.travis-ci.org/outsider/popularconvention.png?branch=master)](http://travis-ci.org/outsider/popularconvention)
 
------------------------
+find popular convention in github commit
+
+## Getting Started
+Install the module with: `npm install popularconvention`
+
+```javascript
+var popularconvention = require('popularconvention');
+popularconvention.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2013 "Outsider" Jeonghoon Byun  
+Licensed under the MIT license.
