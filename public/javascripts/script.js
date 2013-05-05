f788d9a1678367b26c61eafdb799e366901de73e
Outsider <outsideris@gmail.com>
2013-05-05 19:53:46 +0900

    closes #6 : added social sharing button.
    * using original code from their sites.
    * facebook, twitter, google plus

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,36 +3,6 @@
 // <http://outsider.mit-license.org/>
 
 $(document).ready(function() {
-  var dataset = {
-    comma: [1034, 348]
-  };
-
-  var width = 600,
-    height = 400,
-    radius = Math.min(width, height) / 2;
-
-  var color = d3.scale.ordinal()
-    .range(['#E74C3C', '#F1C40F', '#9B59B6', '#2ECC71']);
-
-  var pie = d3.layout.pie().sort(null);
-
-  var arc = d3.svg.arc()
-              .innerRadius(radius - 80)
-              .outerRadius(radius - 20);
-
-  var svg = d3.select(".graph .chart").append("svg")
-              .attr("width", width)
-              .attr("height", height)
-              .append("g")
-              .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-  var path = svg.selectAll("path")
-                .data(pie(dataset.comma))
-                .enter().append("path")
-                .attr("fill", function(d, i) { return color(i); })
-                .attr("d", arc);
-  path.append("text");
-
   Handlebars.registerHelper('getScore', function(context) {
     var percentage = context[this.key] / context.total * 100;
     return Math.round(percentage * 1000) / 1000
@@ -41,4 +11,29 @@ $(document).ready(function() {
   Handlebars.registerHelper('getCommitCount', function(commits) {
     return (commits + "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   });
+
+  !function(d,s,id){
+    var js,fjs=d.getElementsByTagName(s)[0];
+    if(!d.getElementById(id)){
+      js=d.createElement(s);
+      js.id=id;
+      js.src="https://platform.twitter.com/widgets.js";
+      fjs.parentNode.insertBefore(js,fjs);
+    }
+  }(document,"script","twitter-wjs");
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/ko_KR/all.js#xfbml=1";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    console.log('gp')
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
 });
