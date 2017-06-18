0607563d7c47f2b687924695da211d75a1344dac
Outsider <outsideris@gmail.com>
2017-06-18 19:44:23 +0900

    add docker for mongodb database of old version

---
+++ data/Dockerfile
@@ -0,0 +1,6 @@
+FROM mongo:3.0.15
+
+ADD . /home
+WORKDIR /home
+
+CMD ["./mongo.sh"]
---
+++ data/mongo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# ref: https://stackoverflow.com/questions/39282957/mongorestore-in-a-dockerfile
+
+# Initialize a mongo data folder and logfile
+mkdir -p /data/db
+touch /var/log/mongodb.log
+chmod 777 /var/log/mongodb.log
+
+# Start mongodb with logging
+# --logpath    Without this mongod will output all log information to the standard output.
+# --logappend  Ensure mongod appends new entries to the end of the logfile. We create it first so that the below tail always finds something
+/entrypoint.sh mongod --logpath /var/log/mongodb.log --logappend &
+
+# Wait until mongo logs that it's ready (or timeout after 60s)
+COUNTER=0
+grep -q 'waiting for connections on port' /var/log/mongodb.log
+while [[ $? -ne 0 && $COUNTER -lt 60 ]] ; do
+    sleep 2
+    let COUNTER+=2
+    echo "Waiting for mongo to initialize... ($COUNTER seconds so far)"
+    grep -q 'waiting for connections on port' /var/log/mongodb.log
+done
+
+# Restore from dump
+mongorestore --drop /home/dump
+
+# Keep container running
+tail -f /dev/null
